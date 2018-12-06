61bacdadd18a4aef96938e4ab5a05cbbc9efa916
Warau <50153991+warau0@users.noreply.github.com>
2018-12-06 17:56:04 +0100

    Sort scores highest to lowest in print command.

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -4,11 +4,13 @@ export default function leaderboard({ send, sheetID, scores } = {}) {
   if (!scores) {
     msg += `https://docs.google.com/spreadsheets/d/${sheetID}`;
   } else {
-    Object.keys(scores).forEach((key, index) => {
-      if (scores.hasOwnProperty(key)) {
-        msg += `**#${index + 1}** ${scores[key].name}: **${scores[key].score}**\n`;
-      }
-    });
+    Object.keys(scores)
+      .sort((a, b) => scores[b].score - scores[a].score)
+      .forEach((key, index) => {
+        if (scores.hasOwnProperty(key)) {
+          msg += `**#${index + 1}** ${scores[key].name}: **${scores[key].score}**\n`;
+        }
+      });
   }
 
   send(msg);
